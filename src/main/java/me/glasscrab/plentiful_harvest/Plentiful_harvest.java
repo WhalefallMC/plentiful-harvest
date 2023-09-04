9c30773f1a5d46213176dd00300a3d3af7310155
Sky <45436884+skyphii@users.noreply.github.com>
2023-09-04 16:18:05 -0400

    Prevent animals from being fed super crops

--- src/main/java/me/glasscrab/plentiful_harvest/Plentiful_harvest.java
+++ src/main/java/me/glasscrab/plentiful_harvest/Plentiful_harvest.java
@@ -1,6 +1,7 @@
 package me.glasscrab.plentiful_harvest;
 
 import me.glasscrab.plentiful_harvest.Listeners.CropBreaks.*;
+import me.glasscrab.plentiful_harvest.Listeners.AnimalFeedEvent;
 import me.glasscrab.plentiful_harvest.Listeners.FarmlandTrampleEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,6 +19,7 @@ public final class Plentiful_harvest extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new BeetrootBreakEvent(manager), this);
         this.getServer().getPluginManager().registerEvents(new NetherWartBreakEvent(manager), this);
         this.getServer().getPluginManager().registerEvents(new FarmlandTrampleEvent(), this);
+        this.getServer().getPluginManager().registerEvents(new AnimalFeedEvent(), this);
 
         this.getLogger().info("Farming plugin has enabled!");
     }

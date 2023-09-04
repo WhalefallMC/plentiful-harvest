6e9a7bc8561d52b8ac052f525b294cc3aae6eb97
JamieOnSootMC <jamie@sootmc.net>
2023-09-04 14:36:54 +0100

    change manager to local

--- src/main/java/me/glasscrab/plentiful_harvest/Plentiful_harvest.java
+++ src/main/java/me/glasscrab/plentiful_harvest/Plentiful_harvest.java
@@ -8,18 +8,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Plentiful_harvest extends JavaPlugin {
 
     public static Plentiful_harvest plentiful_harvest;
-    private Manager manager;
-
     @Override
     public void onEnable() {
-        this.manager = new Manager();
+        Manager manager = new Manager();
         plentiful_harvest = this;
         
-        this.getServer().getPluginManager().registerEvents(new WheatBreakEvent(this.manager), this);
-        this.getServer().getPluginManager().registerEvents(new CarrotBreakEvent(this.manager), this);
-        this.getServer().getPluginManager().registerEvents(new PotatoBreakEvent(this.manager), this);
-        this.getServer().getPluginManager().registerEvents(new BeetrootBreakEvent(this.manager), this);
-        this.getServer().getPluginManager().registerEvents(new NetherWartBreakEvent(this.manager), this);
+        this.getServer().getPluginManager().registerEvents(new WheatBreakEvent(manager), this);
+        this.getServer().getPluginManager().registerEvents(new CarrotBreakEvent(manager), this);
+        this.getServer().getPluginManager().registerEvents(new PotatoBreakEvent(manager), this);
+        this.getServer().getPluginManager().registerEvents(new BeetrootBreakEvent(manager), this);
+        this.getServer().getPluginManager().registerEvents(new NetherWartBreakEvent(manager), this);
         this.getServer().getPluginManager().registerEvents(new FarmlandTrampleEvent(), this);
 
         this.getLogger().info("Farming plugin has enabled!");

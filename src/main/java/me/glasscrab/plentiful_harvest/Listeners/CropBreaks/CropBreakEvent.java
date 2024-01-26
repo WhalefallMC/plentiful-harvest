a43e78505766e11f8cb0ca171602844407481857
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2024-01-26 11:52:56 +0000

    Changed all E to Event
    This is so the files can be better read quickly

--- src/main/java/me/glasscrab/plentiful_harvest/Listeners/CropBreaks/CropBreakEvent.java
+++ src/main/java/me/glasscrab/plentiful_harvest/Listeners/CropBreaks/CropBreakEvent.java
@@ -25,16 +25,21 @@ public class CropBreakEvent implements Listener {
         this.manager = manager;
     }
 
+    /*  Function: When a crop breaks, run the function.
+        Parameters: BlockDropItemEvent e
+        Return: void */
     @EventHandler
-    public void onCropBreak(BlockDropItemEvent e) {
-        if(e.getItems().isEmpty()) return;
-        Player player = e.getPlayer();
+    public void onCropBreak(BlockDropItemEvent event) {
+        if(event.getItems().isEmpty()) return; // If there are no items in inventory, return
+        Player player = event.getPlayer();
 
-        if(!manager.isCropBlock(e.getBlockState().getType())) return;
-        if(e.getBlockState() instanceof Container) return;
+        if(!manager.isCropBlock(event.getBlockState().getType())) return; // If the block is not a crop block, return
+        if(event.getBlockState() instanceof Container) return; // If the block is a container, return
 
         Ageable age = (Ageable) e.getBlockState().getBlockData();
 
+        // If the crop is fully grown and is harvested by a custom hoe, replant the crop
+        // return if the crop isnt fully grown or if the crop is harvested by a non-custom hoe
         if(age.getAge() != age.getMaximumAge()) {
             ItemStack handItem = player.getInventory().getItemInMainHand();
 
@@ -42,23 +47,26 @@ public class CropBreakEvent implements Listener {
                 return;
             }
 
-            manager.replant(e.getBlock(), e.getBlockState().getType(), age, e.getItems());
+            manager.replant(event.getBlock(), event.getBlockState().getType(), age, event.getItems());
             return;
         }
 
+        // If the player inventory is full, send a message to the player and play a sound
         if(manager.isFull(player)){
             player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED+"YOUR INVENTORY IS FULL! YOU CANNOT COLLECT SUPER CROPS!"));
             player.playSound(player, Sound.BLOCK_BELL_USE, 0.6f, 1);
         }
 
-        for (Item dropItem : e.getItems()) {
+
+        for (Item dropItem : event.getItems()) {
+            // Chance to get a super crop
             int chance = 300;
             int rand = (int) (Math.random() * chance) + 1;
             int jackpot = 256;
 
             ItemStack handItem = player.getInventory().getItemInMainHand();
             if(manager.isCustomHoeOne(handItem)) {
-                for(Item droppedItem : e.getItems()) {
+                for(Item droppedItem : event.getItems()) {
                     droppedItem.remove();
                     if(manager.isCropSeed(droppedItem.getItemStack().getType())) {
                         droppedItem.getItemStack().setAmount(droppedItem.getItemStack().getAmount()-1);
@@ -67,32 +75,32 @@ public class CropBreakEvent implements Listener {
                     player.getInventory().addItem(droppedItem.getItemStack());
                 }
 
-                Ageable ageable = (Ageable) e.getBlockState().getBlockData();
+                Ageable ageable = (Ageable) event.getBlockState().getBlockData();
                 ageable.setAge(0);
 
-                manager.replantLater(e.getBlock(), e.getBlockState().getType(), ageable);
+                manager.replantLater(event.getBlock(), event.getBlockState().getType(), ageable);
             } else if(manager.isCustomHoeTwo(handItem)) {
-                for(Item droppedItem : e.getItems()){
+                for(Item droppedItem : event.getItems()){
                     droppedItem.remove();
                 }
 
-                Ageable ageable = (Ageable) e.getBlockState().getBlockData();
+                Ageable ageable = (Ageable) event.getBlockState().getBlockData();
                 ageable.setAge(0);
 
-                manager.replantLater(e.getBlock(), e.getBlockState().getType(), ageable);
+                manager.replantLater(event.getBlock(), event.getBlockState().getType(), ageable);
             }
 
             if(rand != jackpot) return;
 
-            player.sendMessage(manager.cropBlockMessage(e.getBlockState().getType()));
+            player.sendMessage(manager.cropBlockMessage(event.getBlockState().getType()));
             List<String> lore = new ArrayList<>();
-            lore.add(ChatColor.GRAY + manager.cropBlockLore(e.getBlockState().getType()));
-            ItemStack superCrop = manager.makeSuperCrop(manager.cropBlockName(e.getBlockState().getType()), manager.cropBlockToItem(e.getBlockState().getType()), lore, 1, 1);
+            lore.add(ChatColor.GRAY + manager.cropBlockLore(event.getBlockState().getType()));
+            ItemStack superCrop = manager.makeSuperCrop(manager.cropBlockName(event.getBlockState().getType()), manager.cropBlockToItem(event.getBlockState().getType()), lore, 1, 1);
 
             if(manager.isCustomHoe(handItem)) {
                 manager.giveSuperCrop(player, superCrop);
             } else {
-                Item superCropItem = e.getItems().get(0).getWorld().dropItem(e.getItems().get(0).getLocation(), superCrop);
+                Item superCropItem = event.getItems().get(0).getWorld().dropItem(event.getItems().get(0).getLocation(), superCrop);
                 superCropItem.setGlowing(true);
             }
         }
